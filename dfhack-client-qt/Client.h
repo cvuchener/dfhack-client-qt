36dc2e75d0fd443d55b84de0c18114ad37dcf888
Clément Vuchener <clement.vuchener@gmail.com>
2023-02-24 18:32:27 +0100

    Make CommandResult compatible with std::error_code

--- dfhack-client-qt/Client.h
+++ dfhack-client-qt/Client.h
@@ -25,12 +25,33 @@
 #include <QThread>
 #include <QFuture>
 
-#include <dfhack-client-qt/globals.h>
+#include <dfhack-client-qt/CommandResult.h>
 #include <dfhack-client-qt/CoreProtocol.pb.h>
+#include <dfhack-client-qt/globals.h>
 #include <dfhack-client-qt/static_string.h>
 
 namespace DFHack
 {
+
+enum class Color
+{
+	Black = 0,
+	Blue = 1,
+	Green = 2,
+	Cyan = 3,
+	Red = 4,
+	Magenta = 5,
+	Brown = 6,
+	Grey = 7,
+	DarkGrey = 8,
+	LightBlue = 9,
+	LightGreen = 10,
+	LightCyan = 11,
+	LightRed = 12,
+	LightMagenta = 13,
+	Yellow = 14,
+	White = 15
+};
 
 using TextNotification = std::pair<DFHack::Color, QString>;
 
@@ -151,5 +172,7 @@ private:
 };
 
 } // namespace DFHack
+
+Q_DECLARE_METATYPE(DFHack::Color);
 
 #endif
